ad3604f4979e1a089938418c0845ca1efe4cea22
zahid4523114 <dewanmohammodzahidhasan@gmail.com>
2022-10-10 23:32:38 +0600

    home-page-header-with-image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import logo from "../../img/sergey-zolkin-_UeY8aTI6d0-unsplash.jpg";
 
 const Home = () => {
-  return <div>this is home</div>;
+  return (
+    <div class="card mb-3 p-0 m-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src={logo} class="img-fluid rounded-start" alt="..." />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h4 class="card-title">Quiz</h4>
+            <p class="card-text">
+              A quiz is a form of game or mind sport in which players attempt to
+              answer questions correctly about a certain or variety of subjects.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
